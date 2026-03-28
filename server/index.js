4d17322f87bcf6c72dec103739976dcafe8151b8
Jason <jason@Jasons-MacBook-Air-M4-Gold.local>
2026-03-29 03:15:00 +0800

    fix: Express 5 wildcard route, use app.use() for SPA fallback

--- server/index.js
+++ server/index.js
@@ -670,7 +670,7 @@ const distPath = path.join(__dirname, '..', 'dist');
 
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get('*', (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 }
